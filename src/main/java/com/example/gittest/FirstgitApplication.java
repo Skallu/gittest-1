c722f8da0831b4a5447e130d91b27823a125a1dc
Anusha Kalyan <anushakalyan@Anushas-MBP.attlocal.net>
2021-03-28 22:57:35 -0500

    feat: test message

--- src/main/java/com/example/gittest/FirstgitApplication.java
+++ src/main/java/com/example/gittest/FirstgitApplication.java
@@ -8,7 +8,7 @@ public class FirstgitApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FirstgitApplication.class, args);
-		System.out.println("hello world");
+		System.out.println("hello world Today");
 	}
 
 }
